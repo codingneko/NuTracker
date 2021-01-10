c2a64126c65cc357d7ed45382c4955b6ba8f5d6a
codingneko <11065075+codingneko@users.noreply.github.com>
2021-01-10 05:26:47 +0100

    Add most bonkable users leaderboard

--- controllers/front/index.js
+++ controllers/front/index.js
@@ -1,11 +1,18 @@
 var helpers = require('../../helpers');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     var loggedInUser = helpers.getLoggedInUser(req);
 
     var users = [];
     req.app.locals.db.get('users').value().forEach(user => {
-        users.push(user.username);
+        var frontUser = {}
+        frontUser.fapCount = req.app.locals.db.get('faps').filter({userId: user.id}).value().length;
+        frontUser.name = user.username;
+        if (users.length < 20) users.push(frontUser);
+    });
+    
+    users.sort((a, b) => {
+        return b.fapCount - a.fapCount
     });
 
     res.render("pages/index", {
