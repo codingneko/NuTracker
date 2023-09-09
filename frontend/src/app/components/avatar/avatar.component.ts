8fac0a4363e2adf2c5e439ecaf418c11c8b77257
CodingNeko <codingneko@gmail.com>
2023-09-10 01:54:29 +0200

    use OnChanges instead of OnInit 👌

--- frontend/src/app/components/avatar/avatar.component.ts
+++ frontend/src/app/components/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 enum AvatarSize {
     MICRO = 20,
@@ -13,20 +13,16 @@ enum AvatarSize {
     templateUrl: './avatar.component.html',
     styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent implements OnInit {
+export class AvatarComponent implements OnChanges {
     @Input()
     avatarUrl = '/assets/avatar.png';
 
     @Input()
     size: AvatarSize = AvatarSize.NORMAL;
 
-    ngOnInit(): void {
-        // This stupid shit is needed because for some reason Angular decided it was a good idea
-        // to load ngOnInit before the @Input, so @Input overwrites this if we don't wait.
-        setTimeout(() => {
-            if (!this.avatarUrl || this.avatarUrl.length < 5) {
-                this.avatarUrl = '/assets/avatar.png';
-            }
-        }, 200);
+    ngOnChanges(): void {
+        if (!this.avatarUrl || this.avatarUrl.length < 5) {
+            this.avatarUrl = '/assets/avatar.png';
+        }
     }
 }
